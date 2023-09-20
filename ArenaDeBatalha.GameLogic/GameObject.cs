7b01afff10a22bb42e84bcb7479cddfe3de83424
ueberjames <ueberjames@gmail.com>
2023-09-19 21:30:45 -0300

    Definindo as  Propriedades do jogo

--- ArenaDeBatalha.GameLogic/GameObject.cs
+++ ArenaDeBatalha.GameLogic/GameObject.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +11,19 @@ namespace ArenaDeBatalha.GameLogic
 {
     public class GameObject
     {
+        public Bitmap Sprite { get; set; }
+        public bool Active { get; set; }
+        public int Speed { get; set; }
+        public int Left { get; set; }
+        public int Top { get; set; }
+        public int Width { get { return this.Sprite.Width; } }
+        public int Height { get { return this.Sprite.Height; } }
+        public Size Bounds { get; set; }
+        public Rectangle Rectangle { get; set; }
+        public Stream Sound { get; set; }
+        public Graphics Screen { get; set; }
+
+        private SoundPlayer soundPlayer { get; set; }
 
     }
 }
